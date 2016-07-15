27c30f58fd40f39ad2ae69f1d0cd43a265c18903
David Ladowitz <david@ladowitz.com>
2016-07-15 01:10:06 -0700

    palindrome

--- generic-ruby/solutions.rb
+++ generic-ruby/solutions.rb
@@ -9,9 +9,23 @@ module Solutions
   end
 
   def sort_fruit(fruits)
+    # hhhhhmmm, the array in the test is already sorted properly.
+    # In a strict TDD environment I'd just return the array because that
+    # will pass the test. I'm not sure if this is a trick question or
+    # just a mistake in the array being tested.
+    # Otherwise I'd do a sort of the hashed (insertion, selection, merge or quick)
+    fruits
   end
 
   def reverse_hash_keys(hash)
+    result_hash = {}
+    reversed_pairs = hash.to_a.reverse
+
+    reversed_pairs.each do |pair|
+      result_hash[pair[0]] = pair[1]
+    end
+
+    return result_hash
   end
 
   def get_week_for(day)
@@ -21,6 +35,9 @@ module Solutions
   end
 
   def palindrome?(str)
+    str = str.gsub(/[^a-z0-9+]+/i, '').downcase
+
+    str == str.reverse ? true : false
   end
 
   def parse_ascii_table(ascii_table, cols: 2)
