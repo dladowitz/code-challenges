73aeb84d4d76b8e0cc1fd2dfda4635c52fb769d1
David Ladowitz <david@ladowitz.com>
2016-07-15 19:51:55 -0700

    parse_ascii_table working

--- generic-ruby/solutions.rb
+++ generic-ruby/solutions.rb
@@ -1,6 +1,7 @@
 require 'date'
 require_relative 'week'
 require_relative 'row'
+require 'pry'
 
 module Solutions
   def greater_than_avg(numbers)
@@ -53,6 +54,14 @@ module Solutions
   end
 
   def parse_ascii_table(ascii_table, cols: 2)
+    puts ascii_table
+
+    headers = find_headers(ascii_table)
+    rows = create_rows(ascii_table, headers)
+    rows.shift 1
+
+    return rows
+
   end
 end
 
@@ -69,3 +78,92 @@ def find_values_greater_than(numbers, avg_value)
   numbers.each {|num| results << num if num > avg_value}
   return results
 end
+
+def find_headers(ascii_table)
+  split_table = ascii_table.split " "
+  headers = []
+  row_divider_count = 0
+
+  split_table.each do |element|
+    if element == "+------------------------------------+-----------------------------------+"
+      row_divider_count += 1
+    end
+
+    if row_divider_count >= 2
+      break
+    end
+
+    unless element == "|" || element == "+------------------------------------+-----------------------------------+"
+      headers << element
+    end
+  end
+
+  return headers
+end
+
+def create_rows(ascii_table, headers)
+  rows = []
+  cells = []
+  ascii_rows = ascii_table.split "\n"
+
+  #iterate over all lines of text
+  ascii_rows.each.with_index do |row, row_index|
+
+    #skip first row
+    if row_index == 0
+      next
+    #if end of row, turn cells into row
+    #push row to rows.
+    elsif row[0] == "+"
+      puts "Createing Rows >>>>>>>>>>>>>"
+      row = Row.new
+      headers.each.with_index do |header, index|
+        row["#{header}"] = cells[index].strip
+      end
+      puts "Row: #{row}"
+      puts " "
+      rows << row
+      cells = []
+      next
+
+    # build up individual cells
+    else
+      split_row = row.split "|"
+      split_row.delete ""
+
+      split_row.each.with_index do |cell, index|
+        puts "Cell: #{cell}"
+        if cells[index]
+          cells[index] += "#{cell.strip} "
+        else
+          cells[index] = "#{cell.strip} "
+        end
+        puts "Cells: #{cells}"
+      end
+    end
+  end
+
+  return rows
+end
+
+
+
+
+# like_start_index = 227
+# like_end_index = 261
+# dislike_start_index = 227 + 37
+# dislike_end_index = 261 + 36
+# rows = []
+
+# while ascii_table[like_start_index]
+#   row = Row.new
+#   row.likes = ascii_table[like_start_index..like_end_index].strip
+#   row.dislikes = ascii_table[dislike_start_index..dislike_end_index].strip
+#
+#   rows << row
+#
+#   like_start_index += 150
+#   like_start_index += 150
+#   dislike_start_index += 150
+#   dislike_end_index += 150
+# end
